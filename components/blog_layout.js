c80946ecf2c2b0d7dcec1b08b138255184050ad2
Mitchell Lee <development@zyrn.dev>
2020-06-08 13:36:08 +1000

    Fixed Blog Layout

--- components/blog_layout.js
+++ components/blog_layout.js
@@ -5,7 +5,7 @@ import Head from 'next/head';
 import Menu from '../components/menu';
 
 const name = 'Mitchell Lee'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Zyrn.Dev | Mitchell Lee'
 
 export default function Layout({ children, home }) {
   return (
@@ -16,13 +16,11 @@ export default function Layout({ children, home }) {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="Learn how to build a personal website using Next.js"
+            content="Zyrn.Dev - we have file sharing, blog posts and more coming soon!"
           />
           <meta
             property="og:image"
-            content={`https://og-image.now.sh/${encodeURI(
-              siteTitle
-            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+            content="/images/pfp.png"
           />
           <meta name="og:title" content={siteTitle} />
           <meta name="twitter:card" content="summary_large_image" />
