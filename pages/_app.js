16f6b0ab9541e31f3cd95da4d0d99c48c44a1674
ZyrnDev <development@zyrn.dev>
2021-01-13 20:11:12 +1100

    Added code snippets w/ syntax highlighting

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/global.css';
+import '../styles/prism-okaidia.css';
+// import '../styles/prism-tomorrow.css';
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
