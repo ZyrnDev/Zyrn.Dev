a849be59c40ca65610c2f02da3833d9483393e23
ZyrnDev <development@zyrn.dev>
2021-06-27 13:06:23 +1000

    Added a desktop menu

--- components/menuContent.tsx
+++ components/menuContent.tsx
@@ -1,33 +1,42 @@
-import React, { FC } from 'react'
+import React, { Children, FC } from 'react'
 import Link from 'next/link';
 import styles from './menuContent.module.css';
 
 const navigation_links = [
-  { name: 'Home', path: "/" },
+  // { name: 'Home', path: "/" },
   { name: 'Blog', path: "/blog" },
   // { name: 'Files', path: "/files" },
   { name: 'Contact', path: "/contact" },
   // { name: 'Test', path: "/test" },
 ];
-const MenuContent: FC<{ closeCallback: () => void, links?: { name: string, path: string }[] }> = ({ closeCallback, links = navigation_links }) => {
-
+const MenuContent: FC<{ closeCallback: () => void, links?: { name: string, path: string }[], desktop?: boolean }> = ({ closeCallback, links = navigation_links, desktop = false }) => {
+  if (desktop) {
+    return (
+      <nav className={styles.menuDesktop}>
+        <MenuLink link={{ name: 'Home', path: "/" }} close={closeCallback}><img src="/favicon.ico" style={{  }}></img></MenuLink>
+        {links.map(link => <MenuLink link={link} close={closeCallback} />)}
+      </nav>
+    )
+  }
   return (
-    <div className={styles.menu}>
-      {links.map(link => <div className={styles['menu-item']} key={link.name}>
-        <Link href={link.path}>
-          <a onClick={closeCallback}>
-            {link.name}
-          </a>
-        </Link>
-      </div>)}
-
+    <nav className={styles.menuMobile}>
+      <MenuLink link={{ name: 'Home', path: "/" }} close={closeCallback} mobile><img src="/favicon.ico" style={{ display: "block", width: "44px", margin: "auto" }} /></MenuLink>
+      {links.map(link => <MenuLink link={link} close={closeCallback} mobile />)}
       <div className={styles.hint}></div>
-    </div>
+    </nav>
   )
 }
 
-// MenuContent.PropTypes = {
-//   closeCallback: React.PropTypes.func.isRequired
-// }
+const MenuLink: FC<{ link: { name: string, path: string }, close: () => void, mobile?: boolean }> = ({ children, link, close, mobile }) => {
+  return (
+    <div className={mobile ? styles['menu-item-mobile'] : styles['menu-item-desktop']} key={link.name}>
+      <Link href={link.path}>
+        <a onClick={close}>
+          {children ? children : link.name}
+        </a>
+      </Link>
+    </div>
+  )
+}
 
 export default MenuContent;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import { FC } from 'react';
 import { GetStaticProps } from 'next';
-import Layout from '@components/layout'
-import utilStyles from '@styles/utils.module.css'
-import { getSortedPostsData, PostMetaData } from '@lib/posts'
-import { BlogPostList } from '@components/posts'
+import Layout from '@components/layout';
+import Image from 'next/image';
+import utilStyles from '@styles/utils.module.css';
+import { getSortedPostsData, PostMetaData } from '@lib/posts';
+import { BlogPostList } from '@components/posts';
+
+export const name = 'Mitchell Lee'
 
 export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
@@ -23,9 +26,11 @@ const meta = {
 
 const Home: FC<{ firstPosts: PostMetaData[] }> = ({ firstPosts }) => {
   return (
-    <Layout meta={meta} home>
-      <section className={utilStyles.headingMd}>
-        <p style={{marginTop: "0.5em", marginBottom: "0"}}>My name is Mitchell 'Zyrn' Lee and I am a 3rd year software engineering student. I'm passionate about building software and have been doing just that for over a decade.</p>
+    <Layout meta={meta} style={{maxWidth: "40rem"}}>
+      <section className="section">
+        <Image src="/images/me.webp" className={utilStyles.borderCircle} alt={name} priority={true} quality={"100%"} width={144} height={144}/>
+        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+        <p className={utilStyles.headingMd}>My name is Mitchell 'Zyrn' Lee and I am a 3rd year software engineering student. I'm passionate about building software and have been doing just that for over a decade.</p>
       </section>
       <BlogPostList posts={firstPosts} style={{}} isRecent={true}/>
     </Layout>
